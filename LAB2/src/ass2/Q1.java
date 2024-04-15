3c521cafa1eb34fefc341b791ea85d246ffb09fe
Priyanshuanand1 <Rajyansh@gmail.com>
2024-04-15 16:03:51 +0530

    Q1

---
+++ LAB2/src/ass2/Q1.java
@@ -0,0 +1,23 @@
+package ass2;
+class person{
+	public String Name;
+	public int age;
+	
+	public person(String Name,int age) {
+		this.Name=Name;
+		this.age=age;
+				
+	}
+}
+public class Q1 {
+	public static void main(String[] args) {
+		person person1= new person("Anand",21);
+		person person2= new person("And",22);
+		
+		System.out.println("name of person1:" +person1.Name);
+		System.out.println("name of person1:" +person1.age);
+		System.out.println("name of person2:" +person2.Name);
+		System.out.println("name of person2:" +person2.age);	
+	}
+
+}
